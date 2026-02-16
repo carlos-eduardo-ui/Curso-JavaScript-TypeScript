e9a59afa3216feb1a22deb65aa2f264f01751204
carlos-eduardo-ui <carloseduardobezerra05@gmail.com>
2026-02-16 14:22:18 -0300

    Mais diferenças sobre Let e Var JS

---
+++ Curso-JavaScript-TypeScript-OtavioMiranda/sessao_3_js_logica_programing/diferenças_var_let_2/49.js
@@ -0,0 +1,43 @@
+const veradeira = true;
+/*
+//let tem escopo de bloco {...bloco};
+//var so tem escopo de funçao
+
+let nome = 'luiz'; //criando
+var nome2 = 'luiz'; 
+
+var nome2 = 'luiz'; //redeclarada
+
+
+if (veradeira) {
+    var nome2 = 'mario'; //redeclarando
+    let nome = 'otavio'; //criando 
+    console.log(nome, nome2);
+
+    if (veradeira) {
+        var nome2 = 'carlos'; //redeclarando
+        let nome = 'edu'; //criando
+        console.log(nome, nome2)
+    }
+}
+
+console.log(nome, nome2); // 'luiz carlos'
+*/
+
+/*
+var sobrenome = 'miranda';
+function falaOi (){
+    console.log(sobrenome);
+    if (veradeira){
+        let nome = 'luiz';
+        console.log(nome)
+    }
+
+}
+
+falaOi();
+*/
+
+console.log(sobrenome);
+var sobrenome = 'miranda'; //undefined
+let sobrenome = 'miranda'; //error 
